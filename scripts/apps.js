d559405b4305f5625234e6e2d4a26f83eeea5fde
Jonathan Wong <jkwong5@gmail.com>
2016-06-21 16:16:33 -0700

    added new json file for map and reduce.

--- scripts/apps.js
+++ scripts/apps.js
@@ -1,139 +1,119 @@
+(function(module){
 //nav show and hide content
-$('nav').on('click', 'a', function(){
-  $('section').hide();
-  var $nav = $(this).data('nav');
-  $('#' + $nav).show();
-});
-
-$('.header').on('click', 'h2', function(){
-  $('section').show();
-});
-
-$(window).on('load', function(){
-  $('section').hide();
-  $('#projects').show();
-  $('#aboutMe').show();
-});
-
-// // constructor functions and Prototype
-// var projects = [];
-// var skills = [];
-// var learnings = [];
-//
-// function Project(me){
-//   this.name = me.name;
-//   this.time = me.time;
-//   this.blurb = me.blurb;
-//   this.category = me.category;
-//   this.status = me.status;
-//   this.pic = me.pic;
-//   this.link = me.link;
-// }
-//
-// function Skill (opts) {
-//   this.title = opts.title;
-//   this.skills = opts.skills;
-// }
-//
-// function Learning (opts) {
-//   this.title = opts.title;
-//   this.body = opts.body;
-// }
-
-//create template
-
-// templateView.create = function(){
-//   var learning;
-//   $('#learnings').empty();
-//
-//   // learning = new Learning({
-//   //   title: $('#learning-title').val(),
-//   //   body: $('#learning-body').val()
-//   // });
-//
-//   $('#learnings').append(learning.toHtml());
-//
-// };
-
-//viewMore button
-var viewMore = {};
-viewMore.teasers = function(){
-  $('.blurb *:nth-of-type(n+1)').hide();
-  $('#project-template').on('click', 'a.more', function(e) {
-    e.preventDefault();
-    $(this).parent().find('*').fadeIn();
-    $(this).hide();
+  $('nav').on('click', 'a', function(){
+    $('section').hide();
+    var $nav = $(this).data('nav');
+    $('#' + $nav).show();
   });
-};
 
-//Status icons for each project
-$(function(){
-  $('span:contains("In Progress")').addClass('icon-play2').text('');
-  $('span:contains("On Hold")').addClass('icon-pause').text('');
-  $('span:contains("Completed")').addClass('icon-notification').text('');
-  $('span:contains("Cancelled")').addClass('icon-cancel-circle').text('');
-});
+  $('.header').on('click', 'h2', function(){
+    $('section').show();
+  });
 
-//Filter for category and status
-var catView = {};
+  $(window).on('load', function(){
+    $('section').hide();
+    $('#projects').show();
+    $('#aboutMe').show();
+  });
 
-catView.populateFilter = function(){
-  $('article').each(function(){
-    if(!$(this).hasClass('projects')){
-      val = $(this).attr('data-category');
-      var optionTag = '<option value="' + val + '">' + val + '</option>';
-      if($('#catfilter option[value="' + val + '"]').length === 0){
-        $('#catfilter').append(optionTag);
+  //viewMore button
+  var viewMore = {};
+  viewMore.teasers = function(){
+    $('.blurb *:nth-of-type(n+1)').hide();
+    $('#project-template').on('click', 'a.more', function(e) {
+      e.preventDefault();
+      $(this).parent().find('*').fadeIn();
+      $(this).hide();
+    });
+  };
+
+  //Status icons for each project
+  $(function(){
+    $('span:contains("In Progress")').addClass('icon-play2').text('');
+    $('span:contains("On Hold")').addClass('icon-pause').text('');
+    $('span:contains("Completed")').addClass('icon-notification').text('');
+    $('span:contains("Cancelled")').addClass('icon-cancel-circle').text('');
+  });
+
+  //Filter for category and status
+  var catView = {};
+
+  catView.populateFilter = function(){
+    $('article').each(function(){
+      if(!$(this).hasClass('projects')){
+        val = $(this).attr('data-category');
+        var optionTag = '<option value="' + val + '">' + val + '</option>';
+        if($('#catfilter option[value="' + val + '"]').length === 0){
+          $('#catfilter').append(optionTag);
+        }
       }
-    }
+    });
+  };
+  //Fetch filters
+  catView.handleFilter = function(){
+    $('#catfilter').on('change', function(){
+      if ($(this).val()) {
+        $('article').hide();
+        $('article[data-category="' + $(this).val() + '"]').show();
+      }
+      else {
+        $('article').fadeIn();
+        $('article.projects').hide();
+      }
+    });
+  };
+
+  //copyright
+  var d = new Date();
+  var y = d.getFullYear();
+  document.getElementById('copy').innerHTML = y;
+
+
+  var templateView = {};
+  var projectView = {};
+  var skillView = {};
+  var resumeView = {};
+
+  templateView.initIndexPage = function(){
+    Learning.all.forEach(function(a){
+      $('#classData').append(a.toHtml());
+    });
+  };
+
+  projectView.initIndexPage = function(){
+    Project.all.forEach(function(a){
+      $('#projectData').append(a.toHtml());
+    });
+  };
+
+  skillView.initIndexPage = function(){
+    Skill.all.forEach(function(b){
+      $('#skillData').append(b.toHtml());
+    });
+  };
+
+  resumeView.initIndexPage = function(){
+    var templateSource = $('#resume-template').html();
+    var template = Handlebars.compile(templateSource);
+    Resume.numYearsByCat().forEach(function(stat){
+      $('.resume-stats').append(template(stat));
+    });
+    $('#resume .years').text(Resume.numYearsAll);
+    $('#resume .cat').text(Resume.all.length);
+  };
+
+  //call functions
+  $(document).ready(function() {
+    catView.populateFilter();
+    catView.handleFilter();
+    viewMore.teasers();
+    // templateView.create();
   });
-};
-//Fetch filters
-catView.handleFilter = function(){
-  $('#catfilter').on('change', function(){
-    if ($(this).val()) {
-      $('article').hide();
-      $('article[data-category="' + $(this).val() + '"]').show();
-    }
-    else {
-      $('article').fadeIn();
-      $('article.projects').hide();
-    }
-  });
-};
 
-//copyright
-var d = new Date();
-var y = d.getFullYear();
-document.getElementById('copy').innerHTML = y;
-
-
-var templateView = {};
-var projectView = {};
-var skillView = {};
-
-templateView.initIndexPage = function(){
-  Learning.all.forEach(function(a){
-    $('#classData').append(a.toHtml());
-  });
-};
-
-projectView.initIndexPage = function(){
-  Project.all.forEach(function(a){
-    $('#projectData').append(a.toHtml());
-  });
-};
-
-skillView.initIndexPage = function(){
-  Skill.all.forEach(function(b){
-    $('#skillData').append(b.toHtml());
-  });
-};
-
-//call functions
-$(document).ready(function() {
-  catView.populateFilter();
-  catView.handleFilter();
-  viewMore.teasers();
-  // templateView.create();
-});
+  module.catView = catView;
+  module.viewMore = viewMore;
+  module.resumeView = resumeView;
+  module.skillView = skillView;
+  module.projectView = projectView;
+  module.templateView = templateView;
+})(window);
